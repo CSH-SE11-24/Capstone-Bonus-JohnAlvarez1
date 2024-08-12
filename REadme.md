ae66b01520297947ff9035cd9c5dfbc0be421ede
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 16:59:27 -0400

    Initial Commit

---
+++ REadme.md
@@ -0,0 +1,5 @@
+My website is baout transformer and it for user who want to gain more information about a certain charecter. The feature I am most proud of is the hover over feature which pop up a discription about a charecter you hover over. If I had more time I would like to add a page and give thanks on the outside resource I used.  accessibility feature I added was the light mode and dark mode for user prefrence. My peer told me to make it more stylish so I added more image and styling like changing text color or fonts.
+
+
+
+
---
+++ auto.js
@@ -0,0 +1,199 @@
+let picture = document.querySelector("#oto")
+let pTag = document.querySelector("#p1")
+
+picture.addEventListener("mouseover", function(event) {
+  pTag.textContent = "Optimus Prime, originally named Orion Pax, began his life as a dock-worker during the Golden Age of Cybertron around nine million years ago. He worked at a storage yard, managing energy shipments. Orion was later transformed into Optimus Prime, the last of the Thirteen Primes, created by Primus to defeat the cosmic threat Unicron.  As the leader of the Autobots, a faction of Transformers opposing the Decepticons, Optimus Prime is characterized by his strong moral integrity and heroism, consistently opposing the Decepticon leader, Megatron. His motto, “Freedom is the right of all sentient beings,” underscores his dedication to justice and liberty."
+})
+
+picture.addEventListener("mouseout", function(event) {
+  pTag.textContent = ""
+})
+
+
+
+
+//when you hover over the card it would pop up a description of the charecter background.
+
+
+let picture2 = document.querySelector("#oto2")
+let pTag2 = document.querySelector("#p2")
+
+picture2.addEventListener("mouseover", function(event) {
+  pTag2.textContent = "Bumblebee is a member of the Autobots, a group of sentient, self-configuring, modular extraterrestrial robotic lifeforms. In the original toy line and animated series, he transforms into a small yellow Volkswagen Beetle. In the live-action movies, he appears as vehicles inspired by Chevrolet American muscle cars, such as a yellow Camaro with black racing stripes. As Optimus Prime’s trusted lieutenant, Bumblebee is known for his luck, determination, and bravery, despite not being the strongest Autobot. His small size allows him to navigate places larger robots cannot. Over the years, he has taken various forms, including a 1977 second-generation Camaro and a fifth-generation Camaro, notably in the 2018 film Bumblebee. "
+
+
+})
+
+picture2.addEventListener("mouseout", function(event) {
+  pTag2.textContent = ""
+})
+
+
+
+
+
+
+
+let picture3 = document.querySelector("#oto3")
+let pTag3 = document.querySelector("#p3")
+
+picture3.addEventListener("mouseover", function(event) {
+  pTag3.textContent = "Grimlock is the leader of the Dinobots, a subfaction of Autobots, and can transform into a metal Tyrannosaurus rex. Known for his third-person speech, Grimlock is simpleminded, aggressive, and powerful, with a disposition that leans more toward the Decepticons' values of strength over wisdom and compassion. Despite frequent disagreements, he respects Optimus Prime’s leadership and strength. Grimlock's immense power allows him to stand toe-to-toe with both Optimus Prime and Megatron. His chaotic behavior often clashes with Prowl, an uptight Autobot. Grimlock’s raw power and unique personality make him a memorable part of the Transformers saga."
+})
+
+picture3.addEventListener("mouseout", function(event) {
+  pTag3.textContent = ""
+})
+
+
+
+
+
+
+
+
+let picture4 = document.querySelector("#oto4")
+let pTag4 = document.querySelector("#p4")
+
+picture4.addEventListener("mouseover", function(event) {
+  pTag4.textContent = "Ironhide is one of the oldest and toughest Autobots, known for his gruff and ornery demeanor and preference for actions over words. Despite his rough exterior, he remains optimistic and upbeat. His distinctive weapon is a water gun capable of shooting various liquids, including liquid nitrogen and acids. In the Generation 1 continuity, Ironhide has participated in many battles and survived the Great War. He can sometimes combine with other Autobots to form powerful combinations like Optimus Maximus or Betatron. In the live-action films, Ironhide serves as the Autobot weapons specialist and is an old friend of Optimus Prime. He is resilient and never backs down, even in the face of tough odds."
+})
+
+picture4.addEventListener("mouseout", function(event) {
+  pTag4.textContent = ""
+})
+
+
+
+
+
+
+
+let picture5 = document.querySelector("#oto5")
+let pTag5 = document.querySelector("#p5")
+
+picture5.addEventListener("mouseover", function(event) {
+  pTag5.textContent = "Jazz is an Autobot from the Generation 1 continuity, specializing in Special Ops and serving as Optimus Prime’s right-hand man. Unlike the more rigid military strategist Prowl, Jazz thrives on adaptability and improvisation, handling whatever surprises the Decepticons throw his way. His upbeat and outgoing personality, combined with a love for Earth music and a knack for incorporating human slang into his vocabulary, make Jazz a beloved and popular member of the Ark's crew."
+})
+
+picture5.addEventListener("mouseout", function(event) {
+  pTag5.textContent = ""
+})
+
+
+
+
+
+
+let picture6 = document.querySelector("#oto6")
+let pTag6 = document.querySelector("#p6")
+
+picture6.addEventListener("mouseover", function(event) {
+  pTag6.textContent = "Jetfire, also known as Skyfire, is an Autobot with a strong scientific inclination and a cutting-edge chassis that reflects the advanced technology he studies. Originally a Decepticon, Jetfire now fights alongside the Autobots, firmly believing that science aligns with truth and liberty. In the live-action films, Jetfire is depicted as a former Decepticon who defects to the Autobots. He is also a playable character in various Transformers video games, including Transformers: Revenge of the Fallen."
+})
+
+picture6.addEventListener("mouseout", function(event) {
+  pTag6.textContent = ""
+})
+
+
+
+
+
+let picture7 = document.querySelector("#oto7")
+let pTag7 = document.querySelector("#p7")
+
+picture7.addEventListener("mouseover", function(event) {
+  pTag7.textContent = "Ratchet is an Autobot from the Generation 1 continuity, serving as the Autobots’ Chief Medical Officer. Known for his irreverent bedside manner and dry wit, he is the go-to bot for any repair needs. While not a formidable warrior, Ratchet’s exceptional healing skills often place him on the front lines of combat, where his unwavering determination shines through. His vehicle mode transforms into an ambulance, and in some universes, he combines with other Autobots to form powerful entities like Defensor, Sky Reign, or Modulus."
+})
+
+picture7.addEventListener("mouseout", function(event) {
+  pTag7.textContent = ""
+})
+
+
+
+
+
+
+let picture8 = document.querySelector("#oto8")
+let pTag8 = document.querySelector("#p8")
+
+picture8.addEventListener("mouseover", function(event) {
+  pTag8.textContent = "Wheeljack is an inventor extraordinaire among the Autobots, known for creating complex devices that often explode, leaving Ratchet or Hoist to handle the aftermath. Despite the frequent mishaps, his occasional breakthroughs have earned him respect, even from the Decepticons. Beyond the lab, Wheeljack is an amazing driver who enjoys showcasing his skills, relying on solid-fuel rocket jets in his forearms for flying. As an Action Master, he uses the dual-mode Turbo Racer for quick transportation. Wheeljack’s blend of genius and chaos makes him a memorable Autobot."
+})
+
+picture8.addEventListener("mouseout", function(event) {
+  pTag8.textContent = ""
+})
+
+
+
+
+
+
+let picture9 = document.querySelector("#oto9")
+let pTag9 = document.querySelector("#p9")
+
+picture9.addEventListener("mouseover", function(event) {
+  pTag9.textContent = "Arcee is a prominent female Autobot, often depicted in pink or blue, and is the most famous of the Female Autobots with numerous incarnations across different continuities. She is a ruthless and deadly warrior, excelling in hand-to-hand combat and sharpshooting, driven by her deep care for her fellow Autobots and the humans they protect. Arcee is keenly aware of the fragility of organic companions and the dangers posed by the Decepticons. Notable moments in her history include fighting in the Battle of Autobot City and the Unicron War in 2005, and binary bonding with Daniel Witwicky to become a Headmaster. Across various timelines, she remains a steadfast ally and key warrior for the Autobots."
+})
+
+picture9.addEventListener("mouseout", function(event) {
+  pTag9.textContent = ""
+})
+
+
+
+
+
+//Selecting elements
+
+
+
+let light = document.querySelector("#light")
+let dark = document.querySelector("#dark")
+let bod1 = document.querySelector("#bd")
+let h1Tag = document.getElementsByClassName("yellow")
+let Ptag = document.getElementsByClassName("black")
+
+
+//Changing the elements when the button is clicked for light or dark mode
+
+light.addEventListener("click", function(event) {
+  for (let i = 0; i < pTag.length; i++) {
+    pTag[i].style.color = "black"
+  }
+
+  for (let x = 0; x < h1Tag.length; x++) {
+    h1Tag[x].style.color = "navy"
+  }
+
+  bod1.style.backgroundColor = "white"
+})
+
+dark.addEventListener("click", function(event){
+  for(let i = 0; i < Ptag.length; i++){
+      Ptag[i].style.color = "white"
+  }
+  for(let x = 0; x < h1Tag.length;x++){
+    h1Tag[x].style.color = "#ffff99"
+  }
+  bod1.style.backgroundColor = "black"
+})
+
+
+
+//Changing to hover over image
+
+
+let GroupA = document.querySelector("#GA")
+
+GroupA.addEventListener("mouseover", function(event){
+  GroupA.src = "AutoPic/Ga.jpg"
+})
+
+
+GroupA.addEventListener("mouseout", function(event){
+  GroupA.src = "AutoPic/Group.jpg" 
+})
